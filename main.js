6260d0a9362beb57ee25fcc45c6dda5811a76857
pedromcunha <pedro.spinsomewebs@gmail.com>
2015-02-01 12:23:32 -0500

    added email validation

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 (function(jQuery) {
 	var nodeAnimated = false;
 
+	function closeModal() {
+		$('.modal-body').animate({
+			top: '0'
+		}, 500);
+		$('.modal-body').slideUp('fast');
+		$('.modal-backdrop').fadeOut('fast');
+		$('body').removeClass('no-scroll');
+	}
+
 	$('.next-btn').on('click', function() {
 		$('html, body').animate({
 	        scrollTop: $('#sectionTwo').offset().top
@@ -16,14 +25,7 @@
 		$('body').addClass('no-scroll');
 	});
 
-	$('.close-modal, .modal-backdrop').on('click', function() {
-		$('.modal-body').animate({
-			top: '0'
-		}, 500);
-		$('.modal-body').slideUp('fast');
-		$('.modal-backdrop').fadeOut('fast');
-		$('body').removeClass('no-scroll');
-	});
+	$('.close-modal, .modal-backdrop').on('click', closeModal);
 
 	$.fn.center = function () {
 	    var left = ( $(window).width() - this.width() ) / 2+$(window).scrollLeft() + "px";
@@ -32,6 +34,18 @@
 	    });
 	    return this;
 	}
+
+	function validateEmail (email) {
+		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	    return re.test(email);
+	}
+
+	$('#mc-embedded-subscribe').on('click', function() {
+		var x = document.forms["mc-embedded-subscribe-form"]["EMAIL"].value;
+	    if (x.length !== 0 && validateEmail(x)) {
+	        closeModal();
+	    }
+	});
 
 	$(document).on('scroll', function() {
 		if($(window).scrollTop() > 1300 && nodeAnimated === false) {
