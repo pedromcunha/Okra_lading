862623f2dc2ecf23665fb955b8174822aae09ea4
pedromcunha <pedro.spinsomewebs@gmail.com>
2015-01-31 19:17:15 -0500

    switched btn animation

--- main.js
+++ main.js
@@ -4,4 +4,15 @@
 	        scrollTop: $('#sectionTwo').offset().top
 	    }, 1000);
 	});
+
+	$('.signup-btn').on('click', function() {
+		$('.modal-backdrop').fadeIn('slow');
+		$('body').addClass('no-scroll');
+	});
+
+	$('.close-modal, .modal-backdrop').on('click', function() {
+		$('.modal-backdrop').fadeOut('slow');
+		$('body').removeClass('no-scroll');
+	});
+
 })();
